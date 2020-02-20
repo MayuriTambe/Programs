81669a47718e1fe075808e41b0fac412896f6ab2
MayuriTambe <tambemayurii190@gmail.com>
2020-02-20 13:04:34 +0530

    ADDED:To check first and last color from list

---
+++ Programs/BasicPython/Colors.py
@@ -0,0 +1,11 @@
+
+def FisrtLast():
+    color_list = ["Red","Green","White" ,"Black"]
+    First=color_list[0]
+    print("The first color is:",First)
+    Last=color_list[-1]
+    print("The Last color is:",Last)
+    return
+FisrtLast()
+
+
