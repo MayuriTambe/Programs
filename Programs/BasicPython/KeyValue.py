0313ec2e144e1a6cef2e4aa13f3e8c02618b1fcd
MayuriTambe <tambemayurii190@gmail.com>
2020-02-21 13:01:02 +0530

    Added:To separate the key and value of dictonaries

---
+++ Programs/BasicPython/KeyValue.py
@@ -0,0 +1,8 @@
+#To separate the key and value of dictonaries
+
+colors={"Red":101}
+print("The Dictonaries of colors is:",colors)
+(key,value),=colors.items()
+
+print("The key of Dictonary is:",key)
+print("The value of Dictonaries:",value)
