5fc79171e26052347cf05b9aa0a07339815f8160
MayuriTambe <tambemayurii190@gmail.com>
2020-02-25 14:26:00 +0530

    ADDED:To Convert array into List and Tuple

---
+++ Programs/Numpy/Conversion.py
@@ -0,0 +1,10 @@
+import numpy as np
+list=[1 ,2 ,3 ,4 ,5, 6 ,7 ,8]
+arr=np.array(list)
+print("Convert list to array is:",arr)
+Tuple=(8,4,6,1,2,3)
+arr1=np.array(Tuple)
+reshape=arr1.reshape(2,3)
+print("Convert Tuple to array is:\n",reshape)
+
+
