a4cee2878c0f30d8c89372e62aaa37e59c96a3a2
MayuriTambe <tambemayurii190@gmail.com>
2020-02-25 14:17:54 +0530

    ADDED:To Append values to the end of array

---
+++ Programs/Numpy/AppendArrays.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array=np.array([10, 20, 30] )
+print("The array before Appending:",array)
+
+new_array=np.array([40,50,70,80,90,100])
+append_array=np.append(array,new_array)
+
+print("The array after Appending:",append_array)
