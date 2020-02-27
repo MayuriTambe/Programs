edcec0d55e96a430625bed59058342d17d1dcd83
MayuriTambe <tambemayurii190@gmail.com>
2020-02-27 10:58:24 +0530

    ADDED:To create multiple plots

---
+++ Programs/Matplotlib/Basics/MultiplePlots.py
@@ -0,0 +1,25 @@
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+fig.subplots_adjust(bottom=0.2, left=0.20, top = 0.700, right=0.500)
+
+plt.subplot(2, 1, 1)
+plt.xticks(()), plt.yticks(())
+
+plt.subplot(2, 3, 4)
+plt.xticks(())
+plt.yticks(())
+
+plt.subplot(2, 3, 5)
+plt.xticks(())
+plt.yticks(())
+
+
+plt.show()
+
+
+
+
+
+
