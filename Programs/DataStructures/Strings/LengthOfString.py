e82445620278ecdd5e9147996944a51b404a33f1
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:56:24 +0530

    ADDED:To calculate the length of a string

---
+++ Programs/DataStructures/Strings/LengthOfString.py
@@ -0,0 +1,6 @@
+Name_of_String=input("Enter the String:")
+counter = 0
+for element in Name_of_String:
+      counter = counter+1
+print("Length of the input string is:", counter)
+Ends = Name_of_String + "ing"
