701ad701a4344731d34a1e8dd62f6aff38d2e072
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 12:55:01 +0530

    ADDED:To find common element from list.

---
+++ Programs/DataStructures/List/12.commonEle.py
@@ -0,0 +1,5 @@
+First_list=["red","green","yellow","pink"]
+Second_list=["black","brown","red","yellow"]
+
+New_list=(set(First_list)).intersection(set(Second_list))
+print("The Common Elements of both list are:",New_list)
