f118fd90764151abf3fa5c5b7a83b8bbb427deae
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:46:17 +0530

    ADDED:To print all unique values in a dictionary

---
+++ Programs/DataStructures/Dictionary/UniqueValues.py
@@ -0,0 +1,6 @@
+#To calculate the unique values
+Data=[{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"},
+      {"V":"S009"},{"VIII":"S007"}]
+
+unique_value = set( val for dic in Data for val in dic.values())
+print("Unique Values: ",unique_value)
