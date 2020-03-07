cc6eb38f58a6495f9897941c462b30f914d833b7
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 15:37:47 +0530

    ADDED:To display formatted text (width=50) as output

---
+++ Programs/DataStructures/Strings/Formatted.py
@@ -0,0 +1,7 @@
+import textwrap
+text = '''
+  Git is a free and open source distributed version control system designed to handle everything
+   from small to very large projects with speed and efficiency.'''
+print()
+print(textwrap.fill(text, width=50))
+print()
