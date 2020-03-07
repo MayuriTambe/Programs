a6aa04cee9b07c9ff792f2474dbae86bef42def2
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:29:07 +0530

    ADDED:To add member(s) in a set.

---
+++ Programs/DataStructures/sets/Add.py
@@ -0,0 +1,8 @@
+subjects={"Maths","Algebra","History","Biology","Mechanics"}
+print("The set of subjects is:",subjects)
+
+subjects.add("Networks")
+subjects.add("physics")
+subjects.add("computer")
+
+print("The new set of subjects is:",subjects)
