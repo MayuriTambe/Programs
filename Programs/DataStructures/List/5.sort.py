0228086b6d418d17a64a11513c7002e343075a61
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 12:46:48 +0530

    ADDED:To sort the elements in increasing order by the last element in each tuple

---
+++ Programs/DataStructures/List/5.sort.py
@@ -0,0 +1,13 @@
+values=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+print("The list is",values)
+
+def lastElement(values):
+    for i in values:
+        i=0
+        return values[-1]
+        
+print(lastElement(values))
+
+
+
+
