ae65b902de680835381c3a4d2b1ebda6d17773da
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:33:56 +0530

    ADDED:To create set difference.

---
+++ Programs/DataStructures/sets/Diffrence.py
@@ -0,0 +1,8 @@
+marks={66,34,76,55,98,57,63}
+weights={45,63,76,89,65,39,48}
+
+Diffrence=marks.difference(weights)
+print("The diffrence using union function is:",Diffrence)
+
+Diffrence=(marks-weights)
+print("The diffrence using - operator is:",Diffrence)
