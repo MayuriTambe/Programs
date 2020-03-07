66c5493613dfb1b651c7808dfaac28437f6af6c0
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:30:09 +0530

    ADDED:To remove member(s) from sets

---
+++ Programs/DataStructures/sets/Remove.py
@@ -0,0 +1,12 @@
+names={"john","lisa","Rose","alexa","marvin","Justin","lili","hannah"}
+print("The set of names is:",names)
+
+#using discard()will not show any error if it is not present
+names.discard("hannah")
+names.discard("Rose")
+names.discard("alexa")
+names.discard("mill")
+print("The set of names after removing is:",names)
+
+
+
