7fd5917a23b9a78e0b65473978aebf922f439b28
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 12:52:16 +0530

    ADDED:To remove elements from position.

---
+++ Programs/DataStructures/List/9.RemovePosition.py
@@ -0,0 +1,8 @@
+list=['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print("The list is;",list)
+
+print(list.pop(0))
+print(list.pop(4))
+print(list.pop(-1))
+
+print("After removing the elements in list:",list)
