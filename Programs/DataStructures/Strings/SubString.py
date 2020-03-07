ea52c4aa88b8092514a158d5e8783f911ec9e1c7
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 15:36:02 +0530

    ADDED:To count occurrences of a substring in a string.

---
+++ Programs/DataStructures/Strings/SubString.py
@@ -0,0 +1,6 @@
+Name_of_String=input("Enter the String")
+print("The String is:",Name_of_String)
+Substring=input("Enter the substring from the string")
+
+Occurence=Name_of_String.count(Substring)
+print(Occurence)
