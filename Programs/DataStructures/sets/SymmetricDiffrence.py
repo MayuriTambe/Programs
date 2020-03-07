42eb44e702a3b764e0cba974e2008e3e75f238d7
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:34:58 +0530

    ADDED:To create a symmetric difference.

---
+++ Programs/DataStructures/sets/SymmetricDiffrence.py
@@ -0,0 +1,7 @@
+set_A = {1, 2, 3, 4, 5}
+set_B = {6, 7, 3, 9, 4}
+diffrence=set_A.symmetric_difference(set_B)
+print("The set using symmetric_diffrence function is:",diffrence)
+
+diffrence=set_A^(set_B)
+print("The set using ^ operator is:",diffrence)
