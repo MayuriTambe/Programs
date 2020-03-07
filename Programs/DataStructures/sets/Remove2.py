48bea84d9cc655a54e4d21153306107b21846a47
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:31:09 +0530

    ADDED:To remove an item from a set if it is present in the set.

---
+++ Programs/DataStructures/sets/Remove2.py
@@ -0,0 +1,8 @@
+numbers={1,2,4,3,5,6,7,8,9}
+print("The set is:",numbers)
+#using remove() it will show error if it is not present
+numbers.remove(1)
+numbers.remove(4)
+numbers.remove(6)
+
+print("The set after removing is:",numbers)
