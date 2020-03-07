2c4595bdc5e3a4285edf59d9a507e1e5687c0384
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:33:02 +0530

    ADDED:To create aa Union Of sets

---
+++ Programs/DataStructures/sets/Union.py
@@ -0,0 +1,8 @@
+marks={66,34,76,55,98,57,63}
+weights={45,63,76,89,65,39,48}
+
+union=marks.union(weights)
+print("The union using union function is:",union)
+
+union=(marks|weights)
+print("The intersection using | operator is:",union)
