db9c440e2c90247248e97055b72ad0cab0f79a2a
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:05:29 +0530

    ADDED:To create a set

---
+++ Programs/DataStructures/sets/CreateSet.py
@@ -0,0 +1,11 @@
+#To create Set
+
+colors={"red","green","yellow","Pink","black"}
+print("The set1 is:",colors)
+print(type(colors))
+
+numbers={1,2,3,5,7,8,9}
+print("The set2 is:",numbers)
+
+Month_year={"januray:2020","february:1996","March:2015"}
+print(Month_year)
