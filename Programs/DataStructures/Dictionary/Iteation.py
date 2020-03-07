135cbd872cb514ed956ebc37d37ff4023d60be75
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 14:42:39 +0530

    ADDED:To iterate over dictionaries using for loops.

---
+++ Programs/DataStructures/Dictionary/Iteation.py
@@ -0,0 +1,11 @@
+#Iteration over Dictionary
+mydict = {"Red":4,"Pink":2,"Yellow":3,"Black":1}
+print("The Dictionary is:",mydict)
+
+#Iteration over keys
+for element in mydict:
+    print(element)
+
+#Iteration over values
+for element in mydict.values():
+    print(element)
