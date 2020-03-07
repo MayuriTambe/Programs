4453d9c04eb9f0c18efb9c7c3e74f22bc5f41b82
MayuriTambe <tambemayurii190@gmail.com>
2020-03-07 12:29:51 +0530

    ADDED:To unpack a tuple in several variables

---
+++ Programs/DataStructures/Tuples/Unpacktuple.py
@@ -0,0 +1,9 @@
+Student_Data=("john",22,"Computer",88593657858)
+print("The student data is:",Student_Data)
+
+(Name,age,branch,phonenum)=Student_Data
+
+print("The name of student is:",Name)
+print("The age of student is",age)
+print("The branch of student is:",branch)
+print("The phone number is:",phonenum)
